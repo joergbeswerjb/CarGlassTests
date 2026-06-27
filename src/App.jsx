9adcfa959bae0590be7b09c680e559148fd0e0e4
joergbeswerjb <joerg.beswer.jb@gmail.com>
2026-06-27 15:36:21 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import StartPage from './pages/StartPage.jsx'
 import TestPage from './pages/TestPage.jsx'
 import DonePage from './pages/DonePage.jsx'
 import HRPage  from './pages/HRPage.jsx'
@@ -8,23 +9,18 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Стартовая страница - выбор роли */}
+        <Route path="/" element={<StartPage />} />
         {/* Тест - роль берётся из URL параметра */}
         <Route path="/test/:roleSlug" element={<TestPage />} />
-
         {/* Экран завершения - кандидат не видит результатов */}
         <Route path="/done" element={<DonePage />} />
-
         {/* HR панель - список кандидатов */}
         <Route path="/hr" element={<HRPage />} />
-
         {/* HR панель - детальная карточка кандидата */}
         <Route path="/hr/:roleSlug/:id" element={<CandidateCardPage />} />
-
-        {/* Редирект с корня */}
-        <Route path="/" element={<Navigate to="/hr" replace />} />
-
-        {/* 404 - на HR */}
-        <Route path="*" element={<Navigate to="/hr" replace />} />
+        {/* 404 - на стартовую (а не в HR) */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
